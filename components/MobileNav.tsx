40baf391ce7669959c46a7de8190e6252686d5d2
Furkan Emre Kozan <kozanfurkanemre@gmail.com>
2025-04-25 21:33:45 +0300

    a

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -82,6 +82,12 @@ const MobileNav = () => {
                     {link.title}
                   </Link>
                 ))}
+                   <a
+                href={"https://photos-emre-kozan.vercel.app/"}
+                className="hover:text-primary-500 dark:hover:text-primary-400 mb-4 py-2 pr-4 text-2xl font-bold tracking-widest text-gray-900 outline outline-0 dark:text-gray-100"
+                >
+                Photo Blog
+              </a>
               </nav>
 
               <button
